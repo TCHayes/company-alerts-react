fe48b8eec890c73bcdf7c0287c19495ec249c46f
Terry Hayes <terrychayes@gmail.com>
2017-04-26 15:17:07 -0400

    add Cookies import

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
+import * as Cookies from 'js-cookie';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
 
